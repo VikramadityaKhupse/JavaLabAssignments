1ed268c72ac899bdf4b4e2b40ca88b7c356f512c
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-01-30 20:48:40 +0530

    Optimized code

--- Correct_reg.java
+++ Correct_reg.java
@@ -1,78 +1,66 @@
-class Correct_reg{
-	public static void main(String[] args){
-		System.out.println(args.length);
-		System.out.println(check_reg(args));
+//Java code to return count of valid registration numbers according to SGGS format
+class Correct_reg {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        System.out.println("Valid registrations count: " + check_reg(args));
+    }
 
-	
-	}
-	
-	public static int check_reg(String[] args){
-		int count = 0;
-		
-		
-		for(int i = 0; i < args.length; i++){
-			if(args[i].length() != 10){
-				continue;
-			}
-			
-			
-			boolean starting_4_digit = true;
-			boolean ending_3_digit = true;
-			boolean middle_3_words = true;
-			
-			for(int j = 0; j < 10; j++){
-			if(j < 3){
-				if(!Character.isDigit(args[i].charAt(j))){
-					starting_4_digit = false;
-				}
-			}
-			
-			if( j > 3 && j < 7){
-				
-				if(branch_check(args[i].substring(4, 7)) == false){
-					middle_3_words = false;				
-				}
-			}
-			
-			if( j > 6){
-				if(!Character.isDigit(args[i].charAt(j))){
-					ending_3_digit = false;
-				}
-			}
-			
-			
-			}
-			if(starting_4_digit && ending_3_digit && middle_3_words){
-				System.out.println(args[i]);
-				count++;
-			
-			}
-			
-			
-			
-		
-		}
-		return count;
-	}
-	
-	private static boolean branch_check(String string){
-		String[] branches = {"bit", "bec", "bpd", "bel", "bcs"};
+    public static int check_reg(String[] args) {
+        int count = 0;
 
-		for(int i = 0; i < 5; i++){
-			if(string.equals(branches[i])){
-			return true;
-				
-			}
-		
-		}
-		return false;
-		
-	
-	}
-	
-		
-	
+        for (String registration : args) {
+            if (registration.length() != 10) {
+                continue;
+            }
+
+            boolean isValid = true;
+
+            for (int j = 0; j < 10; j++) {
+                if (j < 3 && !Character.isDigit(registration.charAt(j))) {
+                    isValid = false;
+                    break;
+                }
+
+                if (j > 3 && j < 7) {
+                    if (!branch_check(registration.substring(4, 7))) {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (j > 6 && !Character.isDigit(registration.charAt(j))) {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid) {
+                System.out.println(registration);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static boolean branch_check(String string) {
+        String[] branches = {"bit", "bec", "bpd", "bel", "bcs"};
+        for (String branch : branches) {
+            if (string.equals(branch)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+		
+	
+	
+	
+		
+	
+
 
 
 
