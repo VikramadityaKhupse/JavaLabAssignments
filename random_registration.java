43ddf2ee4bade3293ce72e6b6488e70c8dc9f890
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-02-13 09:34:27 +0530

    Final code to generate registrtion number

--- random_registration.java
+++ random_registration.java
@@ -1,32 +1,65 @@
-private class CustomRandomGenerator {
-    private static long seed = System.currentTimeMillis();
-    
-   
-    private static final long MULTIPLIER = 1103515245L;
-    private static final long INCREMENT = 12345L;
-    private static final long MODULUS = (long) Math.pow(2, 31);
-    
-    public static int nextInt() {
-        seed = (MULTIPLIER * seed + INCREMENT) % MODULUS;
-        return (int) seed;
+class RegistrationGenerator {
+    private static long time = System.currentTimeMillis();
+
+
+    private static char randChar() {
+        String alphabets = "abcdefghijklmnopqrstuvwxyz"; 
+        return alphabets.charAt((int)(System.currentTimeMillis()%26));
     }
+    private static long randYear() {
+	return 	time % 10000;
+	    
+     }
     
-    public static char nextChar() {
-        int randomInt = nextInt();
-        return (char) ('a' + randomInt % 26);
+    private static long randLastThree() {
+	return 	time % 1000;
+	    
+     }
+    
+    
+    
+    
+
+    private static String generateRegistrationNumber() {
+        String registrationNumber;
+        // Generate initial 4-digit year
+        registrationNumber = Long.toString(randYear());
+
+        // Generate 3 small letters
+        registrationNumber += randChar();
+        registrationNumber += randChar();
+        registrationNumber += randChar();
+
+        // Generate last 3 digits
+        registrationNumber += Long.toString(randLastThree());
+
+        return registrationNumber;
     }
-    
+
     public static void main(String[] args) {
-        
-        for (int i = 0; i < 10; i++) {
-            System.out.print(nextInt() + " ");
+        long startTime, endTime;
+        int[] sizes = {1000, 5000, 20000, 50000};
+        //System.out.println(generateRegistrationNumber());
+
+        for (int size : sizes) {
+            String[] registrations = new String[size];
+
+            
+            startTime = System.currentTimeMillis();
+
+            //Generate registration numbers
+            for (int i = 0; i < size; i++) {
+                registrations[i] = generateRegistrationNumber();
+            }
+
+            
+            endTime = System.currentTimeMillis();
+
+           
+            long elapsedTime = endTime - startTime;
+
+            System.out.println("Time taken to generate " + size + " registration numbers: " + elapsedTime + " milliseconds.");
         }
-        System.out.println();
-        
-        for (int i = 0; i < 10; i++) {
-            System.out.print(nextChar() + " ");
-        }
-        System.out.println();
     }
 }
 
